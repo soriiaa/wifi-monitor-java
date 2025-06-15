88f1e199d6a1a6d9de25a53adde57a9adb161bc2
soriiaa <alex.soriaalcaraz@gmail.com>
2025-06-15 14:22:41 +0200

    Commit 1 - Genero el proyecto con Spring Initializr

---
+++ src/main/java/com/sorxx/wifi_monitor/WifiMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.sorxx.wifi_monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WifiMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WifiMonitorApplication.class, args);
+	}
+
+}
